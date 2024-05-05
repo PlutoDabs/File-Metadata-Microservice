80acaca1e27ceeac45f91e472302b075ebd5b1cf
PlutoDabs <163647279+PlutoDabs@users.noreply.github.com>
2024-05-05 17:29:48 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var cors = require('cors');
+var multer = require('multer');
+require('dotenv').config()
+
+var app = express();
+var upload = multer({ dest: 'uploads/' });
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  var file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
